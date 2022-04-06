dc181812061d7a6378ffd09e67d205582c50ecfa
DaianyNascimento <daiany.cristina@outlook.com>
2022-04-06 11:06:23 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+export default function App() {
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import "../App.css";
+
+function HomePage() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default HomePage;
