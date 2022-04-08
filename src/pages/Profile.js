0465bc31ec47fa5745598861b2de1977409ef4a0
Patricia <patriciasilva@Air-de-Patricia.home>
2022-04-08 17:03:46 +0100

    middle

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -12,6 +12,7 @@ export function Profile() {
     const navigate = useNavigate();
     const { user, removeUserFromContext } = useContext(AuthContext);
     const [allProducts, setAllProducts] = useState([]);
+    const [formIsShown, setFormIsShown] = useState(false);
 
     useEffect(() => {
         if (!user) {
@@ -23,11 +24,10 @@ export function Profile() {
         async function fetchAllProducts() {
             console.log("Fetching all products!");
             try {
-                const { data } = await axios.get(`${API_BASE_URL}/allproducts`);
-                console.log("This is data from Profile axios.get all ", data)
-                //if (!data) return;
-                setAllProducts(data);
-              
+                const { data } = await axios.get(`${API_BASE_URL}/products`);
+
+                if (!data.products) return;
+                setAllProducts(data.products);
             } catch (err) {
                 console.log("We got an error");
                 console.error(err);
@@ -50,15 +50,23 @@ export function Profile() {
         }
     }
 
+    const toggleForm = () => {
+        setFormIsShown(!formIsShown);
+    };
+
     return (
         <div>
             <h1>Profile Page</h1>
             {user && <h2>Welcome, {user.email}</h2>}
             <button onClick={logout}>Logout</button>
 
-            <CreateProduct setAllProducts = {setAllProducts}/>
-            <Delete setAllProducts = {setAllProducts}/>
-            <Update setAllProducts = {setAllProducts}/>
+            <button type="primary" onClick={toggleForm}>
+                {formIsShown ? 'Hide form' : 'Create new product'}
+            </button>
+            {formIsShown && <CreateProduct setAllProducts={setAllProducts} />}
+
+            <Delete setAllProducts={setAllProducts} />
+            <Update setAllProducts={setAllProducts} />
 
         </div>
     );
