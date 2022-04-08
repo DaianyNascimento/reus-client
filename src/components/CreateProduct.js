4a3afe0e379191a6eb803aa8da1719aecb39e6f9
DaianyNascimento <daiany.cristina@outlook.com>
2022-04-08 12:46:58 +0100

    Bug fixed - create product + components

--- src/components/CreateProduct.js
+++ src/components/CreateProduct.js
@@ -2,40 +2,60 @@ import axios from "axios";
 import { useState } from "react";
 import { API_BASE_URL } from "../consts";
 
+export function CreateProduct({ setAllProducts }) {
+  const [newProduct, setNewProduct] = useState({ title: "", description: "", image: "" });
 
-export function CreateProduct({setAllProducts}){
+  const handleFormInput = (event) => {
+    setNewProduct({ ...newProduct, [event.target.name]: event.target.value });
+  };
 
-        const [newProduct, setNewProduct] = useState({ title: "", description: "", image: "" });
+  const handleAddNewProduct = async () => {
+    if (newProduct.title.length === 0 || newProduct.description.length === 0 || newProduct.image.length === 0) {
+      alert("Please enter all required information to create a new product!");
+      return;
+    }
 
-        const handleFormInput = (event) => {
-            setNewProduct({ ...newProduct, [event.target.name]: event.target.value });
-        };
-      
-        const handleAddNewProduct = async () => {
-          if (newProduct.title.length === 0 || newProduct.description.length === 0 || newProduct.image.length === 0) {
-            alert("Please enter all the information required to create a new product!");
-            return;
-          }
-      
-          try {
-            const { data } = await axios.post(`${API_BASE_URL}/profile/`, newProduct);
-           
-            console.log(data);
-             
-            setAllProducts(([oldProducts]) => {
-            return [...oldProducts, data.product];
-            });
-          } catch (error) {
-            console.log("Error in updating the product list on the server!", error);
-          }
-        };
-      
-        return (
-          <div>
-            <input name="title" value={newProduct.title} onChange={handleFormInput} />
-            <input name="description" value={newProduct.description} onChange={handleFormInput} />
-            <input name="image" value={newProduct.image} onChange={handleFormInput} />
-            <button onClick={handleAddNewProduct}>Add Product!</button>
-          </div>
-        );
+    try {
+      const { data } = await axios.post(`${API_BASE_URL}/products`, newProduct);
+      console.log(data);
+      setAllProducts((oldProducts) => {
+        return [...oldProducts, data.product];
+      });
+    } catch (error) {
+      console.log("Error in updating the product list on the server!", error);
+    }
+  };
+
+  return (
+    <div>
+      <label>Title:</label>
+      <input
+        type="text"
+        name="title"
+        autoComplete="title"
+        placeholder="product title"
+        value={newProduct.title}
+        onChange={handleFormInput}
+      />
+      <label>Description:</label>
+      <input
+        type="text"
+        name="description"
+        autoComplete="description"
+        placeholder="product description"
+        value={newProduct.description}
+        onChange={handleFormInput}
+      />
+      <label>Image URL:</label>
+      <input
+        type="text"
+        name="image"
+        autoComplete="image"
+        placeholder="product image"
+        value={newProduct.image}
+        onChange={handleFormInput}
+      />
+      <button onClick={handleAddNewProduct}>Create product!</button>
+    </div>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,28 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from "../consts";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthProviderWrapper";
+
 export function Home() {
+  const navigate = useNavigate();
+  const { user, removeUserFromContext } = useContext(AuthContext);
+
+  const logout = async () => {
+    try {
+      await axios.post(API_BASE_URL + "/auth/logout");
+      removeUserFromContext();
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+      alert("there was an error logging out");
+    }
+  }
+
   return (
     <div>
       <h1>Home Page</h1>
+      {user && <button onClick={logout}>Logout</button>}
     </div>
   );
 }
