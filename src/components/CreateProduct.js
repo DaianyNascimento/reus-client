92ad3e77611590bd87a7eb058332786b2db42d6b
Patricia <patriciasilva@Air-de-Patricia.home>
2022-04-13 16:08:54 +0100

    ant I

--- src/components/CreateProduct.js
+++ src/components/CreateProduct.js
@@ -1,17 +1,28 @@
 import axios from "axios";
 import { useState } from "react";
 import { API_BASE_URL } from "../consts";
+import { Form, Input, Button } from 'antd';
 
 export function CreateProduct({ setAllProducts }) {
-  const [newProduct, setNewProduct] = useState({ title: "", description: "", image: "" });
+  const [newProduct, setNewProduct] = useState({
+    title: "",
+    description: "",
+    image: "",
+  });
   const [formIsShown, setFormIsShown] = useState(false);
+
+  const [form] = Form.useForm();
 
   const handleFormInput = (event) => {
     setNewProduct({ ...newProduct, [event.target.name]: event.target.value });
   };
 
   const handleAddNewProduct = async () => {
-    if (newProduct.title.length === 0 || newProduct.description.length === 0 || newProduct.image.length === 0) {
+    if (
+      newProduct.title.length === 0 ||
+      newProduct.description.length === 0 ||
+      newProduct.image.length === 0
+    ) {
       alert("Please enter all required information to create a new product!");
       return;
     }
@@ -35,42 +46,63 @@ export function CreateProduct({ setAllProducts }) {
 
   return (
     <div>
-      <button type="primary" onClick={toggleForm}>
-        {formIsShown ? 'Cancel' : 'Create new product'}
-      </button>
+      <Button className="btnStyle" type="primary" onClick={toggleForm}>
+        {formIsShown ? "Cancel" : "Create new product"}
+      </Button>
 
       {formIsShown && (
-        <form>
-          <label>Title:</label>
-          <input
-            type="text"
-            name="title"
-            autoComplete="title"
-            placeholder="product title"
-            value={newProduct.title}
-            onChange={handleFormInput}
-          />
-          <label>Description:</label>
-          <input
-            type="text"
-            name="description"
-            autoComplete="description"
-            placeholder="product description"
-            value={newProduct.description}
-            onChange={handleFormInput}
-          />
-          <label>Image URL:</label>
-          <input
-            type="text"
-            name="image"
-            autoComplete="image"
-            placeholder="product image"
-            value={newProduct.image}
-            onChange={handleFormInput}
-          />
-          <button onClick={handleAddNewProduct}>Create product!</button>
-        </form>)}
-
+        <Form
+          form={form}
+          layout="vertical"
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 14 }}
+          style={{ padding: "24px" }}
+        >
+          <Form.Item label="TITLE" required tooltip="This is a required field">
+            <Input
+              type="text"
+              name="title"
+              autoComplete="title"
+              placeholder="product title"
+              value={newProduct.title}
+              onChange={handleFormInput}
+            />
+          </Form.Item>
+          <Form.Item
+            label="DESCRIPTION"
+            required
+            tooltip="This is a required field"
+          >
+            <Input.TextArea
+              showCount
+              maxLength={100}
+              type="text"
+              name="description"
+              autoComplete="description"
+              placeholder="product description"
+              value={newProduct.description}
+              onChange={handleFormInput}
+            />
+          </Form.Item>
+          <Form.Item
+            label="IMAGE URL"
+            required
+            tooltip="This is a required field"
+          >
+            <Input
+              type="text"
+              name="image"
+              autoComplete="image"
+              placeholder="product image"
+              value={newProduct.image}
+              onChange={handleFormInput}
+            />
+          </Form.Item>
+          <Button className="btnStyle" type="primary" onClick={handleAddNewProduct}>
+            Create product!
+          </Button>
+        </Form>
+      )}
     </div>
   );
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from 'antd';
 
 export function LoginForm({
     submitFormAction,
